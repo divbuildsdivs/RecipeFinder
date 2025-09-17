ebca4c2713164a9fbf6a3e339f8331d05ac4c662
divbuildsdivs <datasci.divyosmiley9@gmail.com>
2025-09-17 19:51:49 +0100

    trying to make width of search bar responsive

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
     }
     return (
         <form className="recipe-search-bar" onSubmit={(e)=> {e.preventDefault();}}>
-            <input type="text" className="w-200 h-15 m-5 p-2 border-solid border-black shadow-md rounded-md" placeholder="Search a recipe" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}>
+            <input type="text" className="w-80 lg:w-200 h-15 m-5 p-2 border-solid border-black shadow-md rounded-md" placeholder="Search a recipe" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}>
             </input>
             <button type="Submit" className="w-30 h-15 bg-[#FF2C2C] text-white hover:bg-[#F43378] border-[#FF2C2C] border-white rounded-md border-solid border-2" onClick={searchhandler}> Search </button>
 
