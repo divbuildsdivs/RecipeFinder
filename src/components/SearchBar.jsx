466c1a91ea18d13db144c46a6563682ffc0d9949
divbuildsdivs <datasci.divyosmiley9@gmail.com>
2025-11-30 14:01:25 +0000

    Search on input change

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SearchBar = ({setSearchDone, searchTerm, setSearchTerm}) => {
     const [inputValue, setInputValue] = useState("");
     
+    
 
-    const searchhandler = () => {
-        // const filteredList = recipeList.filter((item) => {
-        //     return item.strMeal.toLowerCase().includes(searchTerm.toLowerCase()) || item.strCategory.toLowerCase().includes(searchTerm.toLowerCase());
-        // });
-        //setFilteredList(filteredList);
-        if(inputValue.trim() !== searchTerm.trim() ){
-            setSearchDone(false);
-            setSearchTerm(inputValue.trim());
-        }
-       
-    }
+    useEffect(()=>{
+        console.log(inputValue);
+        setSearchTerm(inputValue.trim());
+    }, [inputValue]);
+
     const recommendedRecipesHandler = () => {
         if(searchTerm.trim() !== "") {
             setSearchDone(false);
@@ -22,11 +17,9 @@ const SearchBar = ({setSearchDone, searchTerm, setSearchTerm}) => {
         }
     }
     return (
-        <form className="recipe-search-bar" onSubmit={(e)=> {e.preventDefault();
-        searchhandler();}}>
-            <input type="text" className="w-80 lg:w-200 h-15 m-5 p-2 border-solid border-black shadow-md rounded-md" placeholder="Search a recipe" value={inputValue} onChange={(e) => setInputValue(e.target.value)}>
+        <form className="recipe-search-bar flex flex-col justify-center items-center" >
+            <input type="text" className="w-[80%] h-15 m-5 p-2 border-solid border-black shadow-md rounded-md" placeholder="Search a recipe" value={inputValue} onChange={(e) =>setInputValue(e.target.value)}>
             </input>
-            <button type="submit" className="px-7 py-4 ml-2 bg-[#FF2C2C] text-white hover:bg-[#F43378] border-[#FF2C2C] border-white rounded-md border-solid border-2 cursor-pointer" > Search </button>
             <button type="button" className="px-7 py-4 ml-2 bg-[#FF2C2C] text-white hover:bg-[#F43378] border-[#FF2C2C] border-white rounded-md border-solid border-2 cursor-pointer" onClick={recommendedRecipesHandler}> Recommended Recipes </button>
         </form>
     );
